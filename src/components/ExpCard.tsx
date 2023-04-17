c186a2ac303fd31717c4825eb14b6c2e0b98211c
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-04-17 14:43:59 +0200

    created ExpCard and ExpCardInterface

---
+++ src/components/ExpCard.tsx
@@ -0,0 +1,9 @@
+import ExpCardInterface from "../types/ExpCardInterface";
+
+const ExpCard = ({ company, position, employmentType,
+    startDate, endDate, location, skills
+}: ExpCardInterface) => {
+    return <h2>ExpCard</h2>;
+}
+
+export default ExpCard;
---
+++ src/types/ExpCardInterface.tsx
@@ -0,0 +1,11 @@
+interface ExpCardInterface {
+    company: string;
+    position: string;
+    employmentType: string;
+    startDate: string;
+    endDate: string;
+    location: string;
+    skills: string[];
+}
+
+export default ExpCardInterface;
