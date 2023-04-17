e7ab0440ddfc2dadb5c55841f42d89803a083904
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-04-18 00:32:42 +0200

    added container with pageContainer class

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,13 @@
+import { Container, Row, Col } from "react-bootstrap";
+
 const Home = () => {
-  return <></>;
+  return (
+    <Container className="pageContainer" fluid>
+      <Row>
+
+      </Row>
+    </Container>
+  );
 };
 
 export default Home;
