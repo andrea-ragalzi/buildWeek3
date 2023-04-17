bc2a986ef12cd9443d3d6663a41a27bb923e4689
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-04-17 22:21:00 +0200

    merge from develop

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-=======
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
->>>>>>> 4560d57 (merge)
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
