4e01980cd3512a716c878dc1126a540bd1bd0dac
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-04-18 00:33:00 +0200

    added test structure

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,5 +1,64 @@
+import { Container, Row, Col } from "react-bootstrap";
+import { MyFooter } from './../components/MyFooter'
+
 const Profile = () => {
-  return <></>;
+  return (
+    <Container className="pageContainer" fluid>
+      <Row>
+        <Col xs={12} md={8}>
+          <Row className="g-3">
+            <Col xs={12}>
+              <div style={{ width: '100%', backgroundColor: 'grey', aspectRatio: '4/3' }}>
+                <h2>Hero</h2>
+              </div>
+            </Col>
+            <Col xs={12}>
+              <div style={{ width: '100%', backgroundColor: 'grey', aspectRatio: '4/3' }}>
+                <h2>Esperienza</h2>
+              </div>
+            </Col>
+            <Col xs={12}>
+              <div style={{ width: '100%', backgroundColor: 'grey', aspectRatio: '4/3' }}>
+                <h2>Formazione</h2>
+              </div>
+            </Col>
+            <Col xs={12}>
+              <div style={{ width: '100%', backgroundColor: 'grey', aspectRatio: '4/3' }}>
+                <h2>Competenze</h2>
+              </div>
+            </Col>
+            <Col xs={12}>
+              <div style={{ width: '100%', backgroundColor: 'grey', aspectRatio: '4/3' }}>
+                <h2>Lingue</h2>
+              </div>
+            </Col>
+          </Row>
+        </Col>
+        <Col xs={12} md={4} className="d-none d-md-block">
+          <Row className="g-3">
+            <Col xs={12}>
+              <div style={{ width: '100%', backgroundColor: 'grey', aspectRatio: '4/3' }}>
+                <h2>Esperienza</h2>
+              </div>
+            </Col>
+            <Col xs={12}>
+              <div style={{ width: '100%', backgroundColor: 'grey', aspectRatio: '4/3' }}>
+                <h2>Esperienza</h2>
+              </div>
+            </Col>
+            <Col xs={12}>
+              <div style={{ width: '100%', backgroundColor: 'grey', aspectRatio: '4/3' }}>
+                <h2>Esperienza</h2>
+              </div>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+      <Row>
+        <MyFooter></MyFooter>
+      </Row>
+    </Container>
+  )
 };
 
 export default Profile;
