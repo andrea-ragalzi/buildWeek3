a8bb96a26e03884b990c5d70a828179b4b915e55
DekanisHani <dekanis@outlook.it>
2023-04-17 16:55:23 +0200

    commit 16:55 Dekanis

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './style/App.scss';
+import LinkedinMain from './components/LinkedinMain';
 
 function App() {
   return (
     <div className="App">
       <h1>APP OK</h1>
+      <LinkedinMain />
     </div>
   );
 }
