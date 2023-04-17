fa87d900100432f1be57955fd8911de0ef39c315
Sara-Cb <saramaiden23@gmail.com>
2023-04-17 22:49:21 +0200

    developing

--- src/App.tsx
+++ src/App.tsx
@@ -2,81 +2,13 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./style/App.scss";
-import LinkedinMain from "./components/LinkedinMain";
 
 import { BrowserRouter } from "react-router-dom";
-import ExpCard from "./components/ExpCard";
-import { ExpCardInterface } from "./types/ExpCardTypes";
-
-const exp1: ExpCardInterface = {
-  company: "GEFIT SPA",
-  position: "Software Developer",
-  employmentType: "Full-time",
-  startDate: "Ottobre 2019",
-  endDate: "Novembre 2022",
-  location: "Alessandria, Italy",
-  skills: [
-    "Bootstrap",
-    "CSS",
-    "HTML",
-    "Flask",
-    "Python",
-    "Git",
-    "Linux",
-    "Bash",
-    "C",
-    "MySQL",
-    "Server Microsoft SQL",
-    "Sviluppo front-end",
-    "Sviluppo di software",
-    "Sviluppo di applicazioni",
-    "Progettazione software",
-    "Strutture dati",
-    "Database",
-    "Programmazione",
-    "Debug",
-    "Problem solving",
-    "Lavoro di squadra",
-    "Mentoring",
-    "Informatica",
-    "Container",
-    "Sistemi embedded",
-    "Raspberry",
-  ],
-};
-
-const exp2: ExpCardInterface = {
-  company: "Epicode",
-  position: "Student",
-  employmentType: "Full-time",
-  startDate: "Gennaio 2023",
-  endDate: "Luglio 2023",
-  location: "Remote",
-  skills: ["Bootstrap", "CSS", "HTML"],
-};
-
-const exp3: ExpCardInterface = {
-  company: "Freelance",
-  position: "Web Developer",
-  employmentType: "Full-time",
-  startDate: "Aprile 2023",
-  endDate: "Aprile 2023",
-  location: "Alessandria, Italy",
-  skills: ["Flask", "Python", "Git", "Linux", "Bash", "C"],
-};
-
-
-
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <ExpCard {...exp1} />
-        <ExpCard {...exp2} />
-        <ExpCard {...exp3} />
-        <LinkedinMain />
-      </div>
+      <div className="App"></div>
     </BrowserRouter>
   );
 }
