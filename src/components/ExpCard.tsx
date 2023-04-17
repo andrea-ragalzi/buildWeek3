3d6013571a5d5b54aebcd81dc0d5e8a1ab2536ae
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-04-17 21:45:32 +0200

    .

--- src/components/ExpCard.tsx
+++ src/components/ExpCard.tsx
@@ -1,9 +1,7 @@
-import ExpCardInterface from "../types/ExpCardInterface";
-import { Container, Row, Col } from 'react-bootstrap'
+import { ExpCardInterface, MonthsMap } from "./../types/ExpCardTypes";
+import { Container, Row, Col, Button } from 'react-bootstrap'
+import { useState, useEffect } from "react";
 
-type MonthsMap = {
-    [key: string]: number;
-};
 
 const months: MonthsMap = {
     'gennaio': 1,
@@ -21,7 +19,7 @@ const months: MonthsMap = {
 };
 
 const employmentTime = (startDate: string, endDate: string): string => {
-    console.log(startDate, endDate);
+    
     if (!endDate) {
         return 'Present';
     }
@@ -35,20 +33,50 @@ const employmentTime = (startDate: string, endDate: string): string => {
     const endYear: number = parseInt(endYearStr.toLowerCase());
 
     const yearsDiff = endYear - startYear;
-    const monthsDiff = endMonth - startMonth;
+    const monthsDiff = (endMonth - startMonth) + 1;
     const monthsLabel = monthsDiff === 1 ? 'mese' : 'mesi';
     const yearsLabel = yearsDiff === 1 ? 'anno' : 'anni';
-    return (
-        `${yearsDiff} ${yearsLabel} e ${monthsDiff} ${monthsLabel}`
-    );
+
+    if (!yearsDiff) {
+        return (
+            `${monthsDiff} ${monthsLabel}`
+        );
+    }
+    else {
+        return (
+            `${yearsDiff} ${yearsLabel} e ${monthsDiff} ${monthsLabel}`
+        );
+    }
 };
 
 const ExpCard = ({ company, position, employmentType,
     startDate, endDate, location, skills
 }: ExpCardInterface) => {
+    const [collapseButton, setCollapseButton] = useState<boolean>(false);
+    const [skillsStr, setSkillsStr] = useState<string>(skills.join(' · '));
+    const MAX_LENGHT_SKILLS = 95;
+
+    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+        setCollapseButton(false);
+        setSkillsStr(skills.join(' · '));
+    }
+
+    useEffect(() => {
+        console.log('use effect 1');
+        if (skillsStr.length >= MAX_LENGHT_SKILLS) {
+            setSkillsStr(skillsStr.substring(0, MAX_LENGHT_SKILLS));
+            setCollapseButton(true);
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
     return (
         <Container>
             <Row>
+                <Col>
+                </Col>
+                <Col>
+                </Col>
                 <Col xs={12}>
                     <strong>{position}</strong>
                 </Col>
@@ -59,10 +87,15 @@ const ExpCard = ({ company, position, employmentType,
                     <p>{`${startDate} - ${endDate} · ${employmentTime(startDate, endDate)}`}</p>
                 </Col>
                 <Col xs={12}>
-                    <p>{position}</p>
+                    <p>{location}</p>
                 </Col>
                 <Col xs={12}>
-                    <p>{position}</p>
+                    <p><strong>Competenze: </strong>{skillsStr}
+                        {collapseButton && <span onClick={handleClick}
+                            className={
+                                skillsStr.length >= MAX_LENGHT_SKILLS ? "text-secondary d-inline" : "text-secondary d-none"}
+                        > ...vedi altro</span>}
+                    </p>
                 </Col>
             </Row>
         </Container>
