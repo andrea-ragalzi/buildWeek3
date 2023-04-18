eb8df053958f5237c9037b78ae78dbc9d3eb1a01
Domenico232 <123988959+Domenico232@users.noreply.github.com>
2023-04-18 15:29:53 +0200

    .

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -32,11 +32,7 @@ export const Messages = () => {
             </div>
 
             <ListGroup>
-                <ListGroup.Item>Cras justo odio</ListGroup.Item>
-                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-                <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-                <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+                <ListGroup.Item><img src="https://picsum.photos/50/50" width="30px" height="30px" className="rounded-circle" alt="avatar_small" /></ListGroup.Item>
             </ListGroup>
  
             </div> }
