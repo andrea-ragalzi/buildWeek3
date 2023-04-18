156bd727e91a85d585ea3af740d04200bdca1057
Sara-Cb <saramaiden23@gmail.com>
2023-04-18 13:36:30 +0200

    inserite tutte le actions

--- src/redux/actions/experiences.ts
+++ src/redux/actions/experiences.ts
@@ -3,118 +3,216 @@ import { ActionTypes, Experience, Action } from "./../../types/expReducer";
 
 // definizione delle azioni
 export const getExperiencesRequest = (): Action => ({
-    type: ActionTypes.GET_EXPERIENCES_REQUEST,
+  type: ActionTypes.GET_EXPERIENCES_REQUEST,
 });
 
 export const getExperiencesSuccess = (experiences: Experience[]): Action => ({
-    type: ActionTypes.GET_EXPERIENCES_SUCCESS,
-    payload: experiences,
+  type: ActionTypes.GET_EXPERIENCES_SUCCESS,
+  payload: experiences,
 });
 
 export const getExperiencesFailure = (error: string): Action => ({
-    type: ActionTypes.GET_EXPERIENCES_FAILURE,
-    error: error,
+  type: ActionTypes.GET_EXPERIENCES_FAILURE,
+  error: error,
 });
 
+export const fetchExperiences = (userId: string) => {
+  return async (dispatch: Dispatch<Action>) => {
+    dispatch(getExperiencesRequest());
+
+    try {
+      const response = await fetch(
+        `https://striveschool-api.herokuapp.com/api/profile/${userId}/experiences`
+      );
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      const experiences = await response.json();
+      dispatch(getExperiencesSuccess(experiences));
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        dispatch(getExperiencesFailure(error.message));
+      } else {
+        dispatch(getExperiencesFailure("An unknown error occurred"));
+      }
+    }
+  };
+};
+
+// AGGIUNGERE ESPERIENZA
+
 export const addExperienceRequest = (): Action => ({
-    type: ActionTypes.ADD_EXPERIENCE_REQUEST,
+  type: ActionTypes.ADD_EXPERIENCE_REQUEST,
 });
 
 export const addExperienceSuccess = (experience: Experience): Action => ({
-    type: ActionTypes.ADD_EXPERIENCE_SUCCESS,
-    payload: experience,
+  type: ActionTypes.ADD_EXPERIENCE_SUCCESS,
+  payload: experience,
 });
 
 export const addExperienceFailure = (error: string): Action => ({
-    type: ActionTypes.ADD_EXPERIENCE_FAILURE,
-    error: error,
+  type: ActionTypes.ADD_EXPERIENCE_FAILURE,
+  error: error,
 });
 
+export const addExperience = (userId: string, experience: Experience) => {
+  return async (dispatch: Dispatch) => {
+    dispatch(addExperienceRequest());
+
+    try {
+      const response = await fetch(
+        `https://striveschool-api.herokuapp.com/api/profile/${userId}/experiences/`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(experience),
+        }
+      );
+      const data = await response.json();
+
+      if (response.ok) {
+        dispatch(addExperienceSuccess(data));
+      } else {
+        dispatch(addExperienceFailure(data.error));
+      }
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        dispatch(getExperiencesFailure(error.message));
+      } else {
+        dispatch(getExperiencesFailure("An unknown error occurred"));
+      }
+    }
+  };
+};
+
+// OTTENERE ESPERIENZA SINGOLA
+
 export const getExperienceRequest = (): Action => ({
-    type: ActionTypes.GET_EXPERIENCE_REQUEST,
+  type: ActionTypes.GET_EXPERIENCE_REQUEST,
 });
 
 export const getExperienceSuccess = (experience: Experience): Action => ({
-    type: ActionTypes.GET_EXPERIENCE_SUCCESS,
-    payload: experience,
+  type: ActionTypes.GET_EXPERIENCE_SUCCESS,
+  payload: experience,
 });
 
 export const getExperienceFailure = (error: string): Action => ({
-    type: ActionTypes.GET_EXPERIENCE_FAILURE,
-    error: error,
+  type: ActionTypes.GET_EXPERIENCE_FAILURE,
+  error: error,
 });
 
+export const fetchExperience = (userId: string, experienceId: string) => {
+  return async (dispatch: Dispatch<Action>) => {
+    dispatch(getExperienceRequest());
+
+    try {
+      const response = await fetch(
+        `https://striveschool-api.herokuapp.com/api/profile/${userId}/experiences/${experienceId}`
+      );
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      const experience = await response.json();
+      dispatch(getExperienceSuccess(experience));
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        dispatch(getExperiencesFailure(error.message));
+      } else {
+        dispatch(getExperiencesFailure("An unknown error occurred"));
+      }
+    }
+  };
+};
+
+// MODIFICA ESPERIENZA SINGOLA
+
 export const editExperienceRequest = (): Action => ({
-    type: ActionTypes.EDIT_EXPERIENCE_REQUEST,
+  type: ActionTypes.EDIT_EXPERIENCE_REQUEST,
 });
 
 export const editExperienceSuccess = (experience: Experience): Action => ({
-    type: ActionTypes.EDIT_EXPERIENCE_SUCCESS,
-    payload: experience,
+  type: ActionTypes.EDIT_EXPERIENCE_SUCCESS,
+  payload: experience,
 });
 
 export const editExperienceFailure = (error: string): Action => ({
-    type: ActionTypes.EDIT_EXPERIENCE_FAILURE,
-    error: error,
+  type: ActionTypes.EDIT_EXPERIENCE_FAILURE,
+  error: error,
 });
 
+export const editExperience =
+  (userId: string, experience: Experience) =>
+  async (dispatch: Dispatch<Action>) => {
+    dispatch(editExperienceRequest());
+
+    try {
+      const response = await fetch(
+        `https://striveschool-api.herokuapp.com/api/profile/${userId}/experiences/${experience._id}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(experience),
+        }
+      );
+
+      if (response.ok) {
+        const updatedExperience = await response.json();
+        dispatch(editExperienceSuccess(updatedExperience));
+      } else {
+        const data = await response.json();
+        dispatch(editExperienceFailure(data.error));
+      }
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        dispatch(editExperienceFailure(error.message));
+      } else {
+        dispatch(editExperienceFailure("An unknown error occurred"));
+      }
+    }
+  };
+
 export const deleteExperienceRequest = (): Action => ({
-    type: ActionTypes.DELETE_EXPERIENCE_REQUEST,
+  type: ActionTypes.DELETE_EXPERIENCE_REQUEST,
 });
 
 export const deleteExperienceSuccess = (id: string): Action => ({
-    type: ActionTypes.DELETE_EXPERIENCE_SUCCESS,
-    payload: id,
+  type: ActionTypes.DELETE_EXPERIENCE_SUCCESS,
+  payload: id,
 });
 
 export const deleteExperienceFailure = (error: string): Action => ({
-    type: ActionTypes.DELETE_EXPERIENCE_FAILURE,
-    error: error,
+  type: ActionTypes.DELETE_EXPERIENCE_FAILURE,
+  error: error,
 });
 
-// definizione dei creatori di azioni asincrone
-export const fetchExperiences = (userId: string) => {
-    return async (dispatch: Dispatch<Action>) => {
-        dispatch(getExperiencesRequest());
+export const deleteExperience = (userId: string, experienceId: string) => {
+  return async (dispatch: Dispatch) => {
+    dispatch(deleteExperienceRequest());
 
-        try {
-            const response = await fetch(
-                `https://striveschool-api.herokuapp.com/api/profile/${userId}/experiences`
-            );
-            if (!response.ok) {
-                throw new Error(response.statusText);
-            }
-            const experiences = await response.json();
-            dispatch(getExperiencesSuccess(experiences));
-        } catch (error: unknown) {
-            if (error instanceof Error) {
-                dispatch(getExperiencesFailure(error.message));
-            } else {
-                dispatch(getExperiencesFailure("An unknown error occurred"));
-            }
+    try {
+      const response = await fetch(
+        `https://striveschool-api.herokuapp.com/api/profile/${userId}/experiences/${experienceId}`,
+        {
+          method: "DELETE",
         }
-    };
-};
+      );
 
-export const fetchExperience = (userId: string, experienceId: string) => {
-    return async (dispatch: Dispatch<Action>) => {
-        dispatch(getExperienceRequest());
-
-        try {
-            const response = await fetch(
-                `https://striveschool-api.herokuapp.com/api/profile/${userId}/experiences/${experienceId}`
-            );
-            if (!response.ok) {
-                throw new Error(response.statusText);
-            }
-            const experience = await response.json();
-            dispatch(getExperienceSuccess(experience));
-        } catch (error: unknown) {
-            if (error instanceof Error) {
-                dispatch(getExperiencesFailure(error.message));
-            } else {
-                dispatch(getExperiencesFailure("An unknown error occurred"));
-            }
-        }
+      if (response.ok) {
+        dispatch(deleteExperienceSuccess(experienceId));
+      } else {
+        const data = await response.json();
+        dispatch(deleteExperienceFailure(data.error));
+      }
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        dispatch(getExperiencesFailure(error.message));
+      } else {
+        dispatch(getExperiencesFailure("An unknown error occurred"));
+      }
     }
+  };
 };
--- src/types/expReducer.ts
+++ src/types/expReducer.ts
@@ -17,10 +17,17 @@ export enum ActionTypes {
   DELETE_EXPERIENCE_FAILURE = "DELETE_EXPERIENCE_FAILURE",
 }
 
+export interface Action {
+  type: ActionTypes;
+  payload?: any;
+  error?: string;
+}
+
 // Definiamo i tipi per lo stato globale dell'applicazione
-export interface AppState {
+export interface ExperienceState {
   experiences: Experience[];
-  isLoading: boolean;
+  selectedExperience: Experience | null;
+  loading: boolean;
   error: string | null;
 }
 
@@ -36,99 +43,3 @@ export interface Experience {
   username: string;
   image: string;
 }
-
-// definizione delle interfacce delle azioni
-
-export interface ExperienceState {
-  experiences: Experience[];
-  selectedExperience: Experience | null;
-  loading: boolean;
-  error: string | null;
-}
-
-export interface GetExperiencesRequestAction {
-  type: ActionTypes.GET_EXPERIENCES_REQUEST;
-}
-
-export interface GetExperiencesSuccessAction {
-  type: ActionTypes.GET_EXPERIENCES_SUCCESS;
-  payload: Experience[];
-}
-
-export interface GetExperiencesFailureAction {
-  type: ActionTypes.GET_EXPERIENCES_FAILURE;
-  error: string;
-}
-
-export interface AddExperienceRequestAction {
-  type: ActionTypes.ADD_EXPERIENCE_REQUEST;
-}
-
-export interface AddExperienceSuccessAction {
-  type: ActionTypes.ADD_EXPERIENCE_SUCCESS;
-  payload: Experience;
-}
-
-export interface AddExperienceFailureAction {
-  type: ActionTypes.ADD_EXPERIENCE_FAILURE;
-  error: string;
-}
-
-export interface GetExperienceRequestAction {
-  type: ActionTypes.GET_EXPERIENCE_REQUEST;
-}
-
-export interface GetExperienceSuccessAction {
-  type: ActionTypes.GET_EXPERIENCE_SUCCESS;
-  payload: Experience;
-}
-
-export interface GetExperienceFailureAction {
-  type: ActionTypes.GET_EXPERIENCE_FAILURE;
-  error: string;
-}
-
-export interface EditExperienceRequestAction {
-  type: ActionTypes.EDIT_EXPERIENCE_REQUEST;
-}
-
-export interface EditExperienceSuccessAction {
-  type: ActionTypes.EDIT_EXPERIENCE_SUCCESS;
-  payload: Experience;
-}
-
-export interface EditExperienceFailureAction {
-  type: ActionTypes.EDIT_EXPERIENCE_FAILURE;
-  error: string;
-}
-
-export interface DeleteExperienceRequestAction {
-  type: ActionTypes.DELETE_EXPERIENCE_REQUEST;
-}
-
-export interface DeleteExperienceSuccessAction {
-  type: ActionTypes.DELETE_EXPERIENCE_SUCCESS;
-  payload: string;
-}
-
-export interface DeleteExperienceFailureAction {
-  type: ActionTypes.DELETE_EXPERIENCE_FAILURE;
-  error: string;
-}
-
-export type Action =
-  | GetExperiencesRequestAction
-  | GetExperiencesSuccessAction
-  | GetExperiencesFailureAction
-  | AddExperienceRequestAction
-  | AddExperienceSuccessAction
-  | AddExperienceFailureAction
-  | GetExperienceRequestAction
-  | GetExperienceSuccessAction
-  | GetExperienceFailureAction
-  | EditExperienceRequestAction
-  | EditExperienceSuccessAction
-  | EditExperienceFailureAction
-  | DeleteExperienceRequestAction
-  | DeleteExperienceSuccessAction
-  | DeleteExperienceFailureAction;
