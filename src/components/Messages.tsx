cec20370ada17029bb0901c2c55ce50962bb2df0
Domenico232 <123988959+Domenico232@users.noreply.github.com>
2023-04-18 11:53:47 +0200

    .

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -10,7 +10,7 @@ export const Messages = () => {
         <img src="//" alt="avatar_small" />Messaggi <i className="bi bi-three-dots"></i>  <i className="bi bi-pencil-square"></i> <i className="bi bi-caret-up"></i>
         </div> 
         : 
-    <div className="Uncollapsedmessage" onClick={()=>setOnoff(!onoff)}>big div <i className="bi bi-three-dots"></i>  <i className="bi bi-pencil-square"></i> <i className="bi bi-caret-up"></i> </div>}
+    <div className="Uncollapsedmessage" onClick={()=>setOnoff(!onoff)}>big div <i className="bi bi-three-dots"></i>  <i className="bi bi-pencil-square"></i> <i className="bi bi-caret-down"></i> </div>}
     </div>
     
     
