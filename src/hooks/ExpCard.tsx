d9335102070d5a6b1987b8d8174d1ee04c869f17
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-04-18 10:28:46 +0200

    removed unused import

--- src/hooks/ExpCard.tsx
+++ src/hooks/ExpCard.tsx
@@ -1,5 +1,5 @@
 import { ExpCardInterface, MonthsMap } from "./../types/ExpCardTypes";
-import { Container, Row, Col, Button } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 import { useState, useEffect } from "react";
 
 
