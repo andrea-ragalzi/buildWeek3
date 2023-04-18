eb58d33698145009661b5316de47d2bccadd67d1
Domenico232 <123988959+Domenico232@users.noreply.github.com>
2023-04-18 14:45:53 +0200

    .

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -19,14 +19,14 @@ export const Messages = () => {
         </div> 
         : 
 
-    <div className="Uncollapsedmessage" onClick={()=>setOnoff(!onoff)}>
+    <div className="Uncollapsedmessage">
         <div className="d-flex justify-content-around mt-2">
-               <span> <img src="https://picsum.photos/50/50" width="30px" height="30px" className="rounded-circle" alt="avatar_small" />Messaggistica </span>
+               <span  onClick={()=>setOnoff(!onoff)}> <img src="https://picsum.photos/50/50" width="30px" height="30px" className="rounded-circle" alt="avatar_small" />Messaggistica </span>
             
                  <div> 
                     <i className="bi bi-three-dots Msghover"></i>  
                     <i className="bi bi-pencil-square mx-3 Msghover"></i> 
-                    <i className="bi bi-caret-down Msghover"></i>
+                    <i  onClick={()=>setOnoff(!onoff)} className="bi bi-caret-down Msghover"></i>
                 </div>
             </div>
             </div> }
