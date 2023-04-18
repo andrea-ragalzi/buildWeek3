a02112d4971dd635129b83a6291a9a67226cad31
Sara-Cb <saramaiden23@gmail.com>
2023-04-18 15:35:30 +0200

    store ok

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,14 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
+import experienceReducer from "./../reducers/experienceReducer";
 
+const allReducers = combineReducers({
+  experience: experienceReducer,
+  // Qui puoi aggiungere altri reducers
+});
 const store = configureStore({
-  reducer: {
-    posts: postsReducer,
-    comments: commentsReducer,
-    users: usersReducer
-  }
-})
+  reducer: allReducers,
+});
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch;
