47c4405298378284d0adf144558cfefd37bbca26
Domenico232 <123988959+Domenico232@users.noreply.github.com>
2023-04-18 15:48:38 +0200

    .

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -21,7 +21,7 @@ export const Messages = () => {
         : 
 
     <div className="Uncollapsedmessage">
-        <div className="d-flex justify-content-around mt-2">
+        <div className="d-flex justify-content-around mt-2 border-bottom" style={{height:"35px"}}>
                <span  onClick={()=>setOnoff(!onoff)}> <img src="https://picsum.photos/50/50" width="30px" height="30px" className="rounded-circle" alt="avatar_small" />Messaggistica </span>
             
                  <div> 
@@ -30,9 +30,12 @@ export const Messages = () => {
                     <i  onClick={()=>setOnoff(!onoff)} className="bi bi-caret-down "></i>
                 </div>
             </div>
+            <div>
+                <input type="search" className="my-2"></input>
+            </div>
 
             <ListGroup>
-                <ListGroup.Item className="Msghoverdue"><img src="https://picsum.photos/70/70" width="45px" height="45px" className="rounded-circle floatable" alt="avatar_small" /><div>ciao</div></ListGroup.Item>
+                <ListGroup.Item className="Msghoverdue"><img src="https://picsum.photos/70/70" width="45px" height="45px" className="rounded-circle floatable border-bottom" alt="avatar_small" /><div>ciao</div></ListGroup.Item>
             </ListGroup>
  
             </div> }
