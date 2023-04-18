ec5100db70a8908311d62051909bb6e78bcd75e4
DekanisHani <dekanis@outlook.it>
2023-04-18 19:42:07 +0200

    .

--- src/components/LinkedinMain.tsx
+++ src/components/LinkedinMain.tsx
@@ -2,27 +2,33 @@ import { Col, Row } from "react-bootstrap";
 import ExpCard from "./ExpCard";
 
 const LinkedinMain = () => {
-    return (
-        <>
-        <Row>
-        <Col xs='2'>
-        <img src="https://placekitten.com/g/80" alt="pic" className="rounded-5 mx-4" />
-        </Col>
-        <Col xs='10' className="p-0">
-        <ExpCard
-        company={"Epicode"}
-        position={"Rome"}
-        employmentType={""}
-        startDate={"2 "}
-        endDate={"3 "}
-        location={
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        }
-        skills={["Html", "Css", "JS", "React"]}
-      /></Col></Row>
-          </>
+  return (
+    <>
+      <Row>
+       
+          <Col xs="2">
+            <img
+              src="https://placekitten.com/g/80"
+              alt="pic"
+              className="rounded-5 mx-4"
+            />
+          </Col>
+          <Col xs="9" className="p-0">
+            <ExpCard
+              company={"Epicode"}
+              position={"Rome"}
+              employmentType={""}
+              startDate={"2 "}
+              endDate={"3 "}
+              location={
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+              }
+              skills={["Html", "Css", "JS", "React"]}
+            />
+          </Col>
+      </Row>
+    </>
+  );
+};
 
-    )
-}
-
-export default LinkedinMain
+export default LinkedinMain;
