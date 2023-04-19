48cd15d612685a3c8db10d79dea3fd97833faf98
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-04-19 20:06:40 +0200

    change Experience type

--- src/types/expCardTypes.ts
+++ src/types/expCardTypes.ts
@@ -1,14 +1,16 @@
 export interface Experience {
-    _id: string;
-    role: string;
-    company: string;
-    startDate: string;
-    endDate: string;
-    description: string;
-    area: string;
-    username: string;
-    image: string;
-    skills?: string[];
+  _id?: string, // server generated
+  role: string,
+  company: string,
+  startDate: string,
+  endDate: string | null,							
+  description: string,
+  area: string,
+  username: string,
+  createdAt?: string, // server generated
+  updatedAt?: string | null, // server generated
+  __v?: string,
+  image: string
   }
 
 export type MonthsMap = {
