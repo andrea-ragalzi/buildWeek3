2efd6fa89a5f62f7120147bd035339cf8ac49b01
Sara-Cb <saramaiden23@gmail.com>
2023-04-19 10:27:01 +0200

    fixed expCard links

--- src/components/LinkedinMain.tsx
+++ src/components/LinkedinMain.tsx
@@ -2,27 +2,35 @@ import { Col, Row } from "react-bootstrap";
 import ExpCard from "./ExpCard";
 
 const LinkedinMain = () => {
-    return (
-        <>
-        <Row>
-        <Col xs='2'>
-        <img src="https://placekitten.com/g/80" alt="pic" className="rounded-5 mx-4" />
+  return (
+    <>
+      <Row>
+        <Col xs="2">
+          <img
+            src="https://placekitten.com/g/80"
+            alt="pic"
+            className="rounded-5 mx-4"
+          />
         </Col>
-        <Col xs='10' className="p-0">
-        <ExpCard
-        company={"Epicode"}
-        position={"Rome"}
-        employmentType={""}
-        startDate={"2 "}
-        endDate={"3 "}
-        location={
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        }
-        skills={["Html", "Css", "JS", "React"]}
-      /></Col></Row>
-          </>
+        <Col xs="10" className="p-0">
+          <ExpCard
+            company={"Epicode"}
+            role={"Rome"}
+            startDate={""}
+            endDate={""}
+            area={
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+            }
+            skills={["Html", "Css", "JS", "React"]}
+            _id={""}
+            description={""}
+            username={""}
+            image={""}
+          />
+        </Col>
+      </Row>
+    </>
+  );
+};
 
-    )
-}
-
-export default LinkedinMain
+export default LinkedinMain;
