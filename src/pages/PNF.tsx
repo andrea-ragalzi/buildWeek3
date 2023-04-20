84d21cd27d53bad005127401524f387b01defa8d
Sara-Cb <saramaiden23@gmail.com>
2023-04-20 12:58:47 +0200

    inserted image in PNF

--- src/pages/PNF.tsx
+++ src/pages/PNF.tsx
@@ -1,4 +1,4 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Image, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const PNF = () => {
@@ -6,21 +6,25 @@ const PNF = () => {
     <Container fluid className="container404">
       <Row className="row404-1">
         <Col>
-        <div className="error404">
-          <h1>Page not found</h1>
-          <h3>
-            Uh oh, we can't seem to find the page you're looking for. Try going
-            back to the previous page or see our
-            <a href="#">
-              <strong> Help center </strong>
-            </a>
-            for more information.
-          </h3>
-          <Link to="/" className="bntFeed">Go to your feed</Link>
-        </div>
+          <div className="error404">
+            <h1>Page not found</h1>
+            <h3>
+              Uh oh, we can't seem to find the page you're looking for. Try
+              going back to
+              <Link to={"/"}>
+                <strong> Home </strong>
+              </Link>
+              for more information.
+            </h3>
+            <Link to="/" className="bntFeed">
+              Go to your feed
+            </Link>
+          </div>
         </Col>
-        </Row>
-      <Row className="footer404"> <img src="C:\Users\Dekan\OneDrive\Desktop\buildWeek3\src\assets\img\bg404.png" alt="bg404"/></Row>
+      </Row>
+      <Row className="footer404">
+        <Image src="/img/pagenotfound.png" alt="bg404" />
+      </Row>
     </Container>
   );
 };
