d7c1bc47e23ecf90cac461e226ac392745c0aa39
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-04-20 18:04:52 +0200

    add user property to Post type

--- src/types/feedTypes.ts
+++ src/types/feedTypes.ts
@@ -1,9 +1,11 @@
+import { Profile } from './../types/profileTypes';
 export interface Post {
   _id?: string;
-  username: string;
+  username?: string;
   createdAt?: string;
   text: string;
   image?: string;
+  user?: Profile
 }
 
 // Definiamo i tipi delle azioni
