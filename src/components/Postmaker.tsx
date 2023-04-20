2ecc3f0bbba962ec640a77e2ebb6fc51ce519753
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-04-20 18:03:27 +0200

    dynamic posts data

--- src/components/Postmaker.tsx
+++ src/components/Postmaker.tsx
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux";
 import { RootState, store } from "./../redux/store/store";
 import { Button, Form } from "react-bootstrap";
 import { addPost } from "../redux/actions/feedActions";
+import { FormEvent } from "react";
 
 export const Postmaker = () => {
   const [Photo, setPhoto] = useState(false);
@@ -11,6 +12,7 @@ export const Postmaker = () => {
   const [Event, setEvent] = useState(false);
   const [Article, setArticle] = useState(false);
   const [show, setShow] = useState(false);
+  const [textPost, setTextPost] = useState('');
 
   const myProfile = useSelector((state: RootState) => state.profile.me);
 
@@ -19,10 +21,14 @@ export const Postmaker = () => {
 
   const dispatch = store.dispatch;
 
-  const handleSubmit = (e: Event) => {
-    e.preventDefault();
-    dispatch(addPost());
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const newPost = { text: textPost }
+    dispatch(addPost(newPost));
+    setShow(false);
+    setTextPost('');
   };
+
 
   return (
     <div>
@@ -45,7 +51,6 @@ export const Postmaker = () => {
               setPhoto(!Photo);
             }}
           >
-            {" "}
             <i className="bi bi-image picture"></i> <span>foto</span>
           </button>
           <button
@@ -53,7 +58,6 @@ export const Postmaker = () => {
               setVideo(!Video);
             }}
           >
-            {" "}
             <i className="bi bi-play-btn-fill picture"></i> <span>Video</span>
           </button>
           <button
@@ -61,7 +65,6 @@ export const Postmaker = () => {
               setEvent(!Event);
             }}
           >
-            {" "}
             <i className="bi bi-calendar-date picture"></i> <span>Evento</span>
           </button>
           <button
@@ -69,39 +72,27 @@ export const Postmaker = () => {
               setArticle(!Article);
             }}
           >
-            {" "}
-            <i className="bi bi-journal-text picture"></i>{" "}
+            <i className="bi bi-journal-text picture"></i>
             <span>Scrivi un articolo</span>
           </button>
         </div>
-
-        <Modal show={show} onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Crea un post</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <div className="linkBehave">
-              <img
-                src={myProfile?.image}
-                alt="avatar"
-                className="rounded-circle"
-                width={50}
-                height={50}
-              />
-              <div>
-                <p>
-                  {myProfile?.name} {myProfile?.surname}
-                </p>
-                <p className="text-muted">
-                  <small>Pubblica: Chiunque</small>
-                </p>
-              </div>
-            </div>
-            <Form onSubmit={handleSubmit()}>
+        <Modal show={show} onHide={handleClose}> <Modal.Header closeButton>
+          <Modal.Title>Crea un post</Modal.Title>
+        </Modal.Header>
+          <Modal.Body> <div className="linkBehave">
+            <img src={myProfile?.image}
+              alt="avatar" className="rounded-circle" width={50} height={50} />
+            <div>
+              <p> {myProfile?.name} {myProfile?.surname} </p>
+              <p className="text-muted">
+                <small>Pubblica: Chiunque</small>
+              </p> </div> </div>
+            <Form onSubmit={handleSubmit}>
               <Form.Label>Scrivi qualcosa!</Form.Label>
-              <Form.Control as="textarea" rows={2}></Form.Control>
-              <button type="submit" className="btnBlue">
-                Pubbilca
+              <Form.Control as="textarea" rows={2} value={textPost}
+                onChange={(e) => setTextPost(e.target.value)}
+                placeholder="Inserisci qui il tuo messaggio" />
+              <button type="submit" className="btnBlue"> Pubblica
               </button>
             </Form>
           </Modal.Body>
