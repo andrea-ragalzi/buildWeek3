63477a10510eb20e6dd9daf508d2f05c62f227f5
W0lfBl4d3 <luigicervo99@gmail.com>
2023-04-20 19:07:57 +0200

    Sezione ads e dissezionamento profile

--- src/components/Profilecomponents/Info.tsx
+++ src/components/Profilecomponents/Info.tsx
@@ -2,20 +2,21 @@ import { Col } from "react-bootstrap"
 import BoxInfo from "../BoxInfo"
 import { useSelector } from "react-redux"
 import { RootState } from "../../redux/store/store"
+import { Profile } from "../../types/profileTypes"
 
-export const Info = () => {
+export const Info = ({
+    bio,
+}: Profile) => {
 
-    const profile = useSelector((state: RootState) => state.profile.me);
 
-    console.log(profile)
-    return (<div>{profile ? (
+    return (<div>{bio ? (
         <Col xs={12}>
             <div
                 style={{ width: "100%", backgroundColor: "white" }}
                 className="border border-1 border-secondary rounded-3"
             >
                 <BoxInfo title="Informazioni" />
-                <p className="ms-3">{profile.bio} </p>
+                <p className="ms-3">{bio} </p>
             </div>
         </Col>
     ) : (<></>)}</div>)
