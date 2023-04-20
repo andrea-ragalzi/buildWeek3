904136ac3d77e7d76086d89080f73dd4ec7a94ae
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-04-21 00:37:05 +0200

    fixed style

--- src/components/BoxInfo.tsx
+++ src/components/BoxInfo.tsx
@@ -8,11 +8,10 @@ interface BoxInfoProps {
 
 const BoxInfo = ({ title }: BoxInfoProps) => {
   return (
-    <div className="d-flex justify-content-between py-2 px-3">
-        <h2>{title}</h2>
+    <div className="d-flex justify-content-between py-2 px-3"> 
+
+       <div className="d-flex justify-content-between"> <h2>{title}</h2> </div>
         <div className="p-2 ">
-        <Link to={""}><Icon.PlusLg className="CardIcons mx-3 fs-4 text-dark"/></Link>
-        <Link to={""}><Icon.Pencil className="CardIcons mx-3 fs-4 text-dark"/></Link>
         </div>
      </div>
   );
