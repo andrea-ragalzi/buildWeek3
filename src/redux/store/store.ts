832ca224ce7b3e7f1abc8cbcb18dcb03f476c3cd
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-04-21 13:35:04 +0200

    created reducer image

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -3,11 +3,13 @@ import { combineReducers } from "redux";
 import experienceReducer from "./../reducers/experienceReducer";
 import profileReducer from "../reducers/profileReducer";
 import feedReducer from "../reducers/feedReducer";
+import imageReducer from "../reducers/imageReducer";
 
 const allReducers = combineReducers({
   experience: experienceReducer,
   profile: profileReducer,
   feed: feedReducer,
+  image: imageReducer
 });
 
 export const store = configureStore({
